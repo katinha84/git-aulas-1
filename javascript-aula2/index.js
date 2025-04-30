80b82f74942abff0abb3e7bd499150938297004a
unknown <antoniakfelixsilva@gmail.com>
2025-04-30 15:16:39 -0300

    pastas

---
+++ javascript-aula2/index.js
@@ -0,0 +1,10 @@
+let nome;
+ nome = "katia"; // string
+let idade = 40; // Number
+let fumante = falso; // boolean
+let temhabilitacao = true; // boolean
+
+nome = "thiago";
+
+const pi = 3.145145141414
+alert(pi)
---
+++ javascript-aula3/index.js
@@ -0,0 +1,7 @@
+let numero1 =Number(prompt("digite o primeiro numero")) ;
+let numero2 =Number(prompt("digite  o segundo numero"));
+let resultado = numero1 + numero2;
+//alert(resultado)
+
+
+console.log(resultado);
